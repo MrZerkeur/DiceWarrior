2eb350e6a301649090ab3a582aae8c7617ece8d1
ahugo <hugo.andriamampianina@ynov.com>
2023-11-27 10:29:32 +0100

    update engine.py to work

--- engine.py
+++ engine.py
@@ -1,42 +1,13 @@
-from character import Warrior, Wizard, Character, Thief, Tank
-from dice import Dice
-
-import random
+from fight_handler import FightHandler
+from rich import print
+from rich.panel import Panel
 
 def main():
-    warrior = Warrior("James")
-    thief = Thief("Timmy")
-    
-    cars: list[Character] = [warrior, thief]
-    stats = {}
-    
-    char1 = random.choice(cars)
-    cars.remove(char1)
-    
-    char2 = random.choice(cars)
-    cars.remove(char2)
-    
-    print(char1)
-    print(char2)
-    
-    stats[char1.get_name()] = 0
-    stats[char2.get_name()] = 0
-
-    for i in range(10):
-        print(f"--------------------")
-        print(f"Round n°{i+1}")
-        
-        char1.regenerate()
-        char2.regenerate()
-        while char1.is_alive() and char2.is_alive():
-            char1.attack(char2)
-            char2.attack(char1)
-        if (char1.is_alive()):
-            stats[char1.get_name()] += 1
-        else:
-            stats[char2.get_name()] += 1
-            
-    print(stats)
+    welcome_panel = Panel.fit(f"Choose your gamemode :\n1. Autobattler (make your team and try to beat your opponent)",title="[bold blue]Welcome to DiceWarrior", padding=1, border_style="blue", title_align="left")
+    print(welcome_panel, "")
+    class_list = ["Warrior", "Tank", "Thief", "Wizard"]
+    fight = FightHandler(class_list)
+    fight.start_battle()
             
 if __name__ == "__main__":
     main()
